87c9d5a07bd1afece8e81bc1bccf21f4e9c8f3b3
Emiliano Jimenez Guadarrama <97817511+Emiliano1919@users.noreply.github.com>
2026-05-08 23:44:41 -0400

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        s=[]
+        for i in operations:
+            if i=="C":
+                s.pop()
+            elif i=="D":
+                s.append(s[-1]*2)
+            elif i=="+":
+                s.append(s[-1]+s[-2])
+            else:
+                s.append(int(i))
+        ans=0
+        for k in s:
+            ans+=k
+        return ans
