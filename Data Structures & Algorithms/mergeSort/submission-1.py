885d6894a74cf421a82f1533c8579557162f628a
Emiliano Jimenez Guadarrama <97817511+Emiliano1919@users.noreply.github.com>
2026-05-12 07:30:10 -0400

    Add: mergeSort - submission-1

---
+++ Data Structures & Algorithms/mergeSort/submission-1.py
@@ -0,0 +1,41 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def mergeSort(self, pairs: List[Pair]) -> List[Pair]:
+        if not pairs:
+            return [] #Edge Case
+        return self.mergeSortHelper(pairs, 0, len(pairs) - 1)
+
+    def mergeSortHelper(self, pairs: List[Pair], s: int, e: int) -> List[Pair]:
+        if e - s + 1 <= 1: #Calculating the size
+            return pairs
+        m = (s + e) // 2
+        self.mergeSortHelper(pairs, s, m) #Do not hard code s to 0
+        self.mergeSortHelper(pairs, m + 1, e)
+        self.merge(pairs, s, m, e)
+        return pairs
+
+    def merge(self, pairs: List[Pair], s: int, m: int, e: int) -> None:
+        L = pairs[s:m+1]
+        R = pairs[m+1:e+1] #We need to include the last one so e+1 
+        i, j = 0, 0
+        k = s #Important to keep the k at s and not at 0 because we dont know where we are mergingg
+        while i < len(L) and j < len(R):
+            if L[i].key <= R[j].key:
+                pairs[k] = L[i]
+                i += 1
+            else:
+                pairs[k] = R[j]
+                j += 1
+            k += 1
+        while i < len(L):
+            pairs[k] = L[i]
+            k += 1
+            i += 1
+        while j < len(R):
+            pairs[k] = R[j]
+            k += 1
+            j += 1
