78793feb182dd567bd655734f0618ad4c4e156e9
Emiliano Jimenez Guadarrama <97817511+Emiliano1919@users.noreply.github.com>
2026-05-12 23:43:38 -0400

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        distance=[0]*len(points) #Remember this
+        res=[]
+        for i in range(len(points)):
+            x1=0
+            y1=0
+            x2=points[i][0]
+            y2=points[i][1]
+            distance[i]=(math.sqrt((x1 - x2)**2 + (y1 - y2)**2),points[i])
+        distance.sort() #It will sort lexicoprahically using the first and second part if the first is the same
+        for i in range(k):
+            res.append(distance[i][1])
+        return res
