1dc35aa75f62a01e785a1dcf130f152a06e2fd5a
Emiliano Jimenez Guadarrama <97817511+Emiliano1919@users.noreply.github.com>
2026-08-31 09:25:44 -0400

    Add: sliding-window-maximum - submission-11

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-11.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q=deque()
+        L=R=0
+        res=[]
+        while R in range(len(nums)):
+            while q and nums[R]>nums[q[-1]]: #If useless smaller delete from back
+                q.pop()
+            if q and nums[R]>nums[q[0]]:
+                q.appendleft(R)
+            else:
+                q.append(R)
+            if q[0]<L: # If expired delete the expired big
+                q.popleft()
+            if R-L+1==k: #If valid you add it to result then you move the Left pointer
+                res.append(nums[q[0]])
+                L+=1
+            R+=1
+            
+        return res
